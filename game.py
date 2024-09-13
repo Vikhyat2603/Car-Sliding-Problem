9c4fd6b2ddf39d0398960cffe0d5ca8d571b3007
Vikhyat2603 <vikhyat2603@gmail.com>
2024-09-13 18:30:53 -0400

    reverted to simple game repr

--- game.py
+++ game.py
@@ -27,38 +27,10 @@ class CarSlidingGame:
             repr_arr[x+delx][y+dely] = chr(97+v_i)
             if isTruck:
                 repr_arr[x+delx*2][y+dely*2] = chr(97+v_i)
-                
-        new_repr_arr = ['-'*(2*self.door_col) + ' ' + '-'*(2*(self.m - self.door_col) - 2)]
-        for i in range(self.n):
-            this_row = []
-            for j in range(self.m):
-                if j!=0:
-                    this_row.append('|')
-                this_row.append(repr_arr[i][j])
-            if i!=0:
-                new_repr_arr.append(['-']*(2*self.m-1))
-            new_repr_arr.append(this_row[:])
-        new_repr_arr.append('-'*(2*self.m-1))
-        # for i in new_repr_arr:
-        #     print(i)
 
-        for i in range(1,2*self.n-1-1):
-            for j in range(2*self.m-1):
-                element = new_repr_arr[i][j]
-                if element == '|':
-                    left = new_repr_arr[i][j-1].lower()
-                    right = new_repr_arr[i][j+1].lower()
-                    if left==right and left.isalpha():
-                        new_repr_arr[i][j] = ' '
-                elif element == '-':
-                    up = new_repr_arr[i-1][j].lower()
-                    down = new_repr_arr[i+1][j].lower()
-                    if up==down and up.isalpha():
-                        new_repr_arr[i][j] = ' '
-        # print(new_repr_arr)
-
-        repr_string = '■' + new_repr_arr[0] + '■\n'
-        for row in new_repr_arr[1:-1]:
+        repr_string = ''
+        repr_string += '■' + '-'*self.door_col + ' ' + '-'*(self.m-self.door_col-1) + '■\n'
+        for row in repr_arr:
             repr_string += '|' + ''.join(row) + '|\n'
-        repr_string += '■' + new_repr_arr[-1] + '■'
+        repr_string += '■' + '-'*self.m + '■'
         return repr_string
