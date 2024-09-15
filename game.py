0a214337d83ea1ded8fe33f5d697e8316b3633bf
Vikhyat2603 <vikhyat2603@gmail.com>
2024-09-15 17:05:47 -0400

    updated main and game

--- game.py
+++ game.py
@@ -1,36 +1,99 @@
 class CarSlidingGame:
-    def __init__(self, gridSize, door_col, coords, isTrucks, directions):
+    unit_vectors = [(0, 1), (-1, 0), (0, -1), (1, 0)] # possible directions of movement
+
+    def __init__(self, gridSize, doorCol, redCarIdx, coords, isTrucks, directions):
         self.n, self.m = gridSize
-        self.door_col = door_col
-        self.n_vehicles = len(coords)
-        self.coords = coords
-        self.isTrucks = isTrucks
-        self.directions = directions
+        self.doorCol = doorCol # notes which column the door is in
+        self.nVehicles = len(coords)
+        self.redCarIdx = redCarIdx # stores index of the red car among the given vehicles
+        self.coords = coords # stores the coordinate tuple for the trunk of each vehicle
+        self.isTrucks = isTrucks # stores 1/0 whether each vehicle is a truck or not
+        self.directions = directions # stores direction for each vehicle, 0 is right, 1 is up, 2 is left, 3 is down
 
-        # if self.check_conflict():
-        #     raise ValueError, "Vehicles provided are already in conflict!"
+    def _outsideGrid(self, i, j):
+        '''check whether a coordinate (i,j) is outside the grid.'''
+        return not ((0<=i<=self.n) and (0<=j<=self.m))
 
-    def check_conflict(self):
-        pass
+    def takeAction(self, vehicleIdx, move_direction):
+        '''change the current game state to move vehicle (vehicleIdx) forward/backword according to (move_direction) '''
+        delx, dely = self.unit_vectors[self.directions[vehicleIdx]]
+        delx *= move_direction
+        dely *= move_direction
+        self.coords[vehicleIdx] = (self.coords[vehicleIdx][0] + delx, self.coords[vehicleIdx][1] + dely)
 
-    def __repr__(self):
-        repr_arr = [[' ']*self.m for i in range(self.n)]
+    def createGrid(self):
+        grid = [[' ']*self.m for i in range(self.n)]
 
-        for v_i in range(self.n_vehicles):
+        for v_i in range(self.nVehicles):
             x, y = self.coords[v_i]
             isTruck = self.isTrucks[v_i]
             direction = self.directions[v_i]
 
-            repr_arr[x][y] = chr(65+v_i)
-            unit_vector = [(0, 1), (-1, 0), (0, -1), (1, 0)]
-            delx, dely = unit_vector[direction]
-            repr_arr[x+delx][y+dely] = chr(97+v_i)
-            if isTruck:
-                repr_arr[x+delx*2][y+dely*2] = chr(97+v_i)
+            # if the trunk coordinate is already occupied by another vehicle, conflict found
+            if grid[x][y] != ' ':
+                raise ValueError("Found vehicle Conflict in grid")
+            grid[x][y] = chr(65+v_i) # place upper case letter corresponding to vehicle index, as the trunk marker
 
+            delx, dely = self.unit_vectors[direction] # get change in coordinates corresponding to direction
+            
+            # if the vehicle is jutting out the grid, or clashing with some other already placed vehicle, conflict found
+            if self._outsideGrid(x+delx, y+dely) or (grid[x+delx][y+dely] != ' '):
+                raise ValueError("Found vehicle conflict in grid")
+            grid[x+delx][y+dely] = chr(97+v_i) # set the next cell of the vehicle to the corresponding lower case letter
+
+            # if the vehicle is a truck, repeat this check for the next coordinate
+            if isTruck:
+                if self._outsideGrid(x+delx*2, y+dely*2) or grid[x+delx*2][y+dely*2] != ' ':
+                    raise ValueError("Found vehicle Conflict in grid")
+                grid[x+delx*2][y+dely*2] = chr(97+v_i)
+
+        return grid
+
+    def hasConflict(self):
+        '''check if the game state has a conflict (cars overlapping or moving out of the grid)'''
+        try:
+            self.createGrid()
+            return True
+        except ValueError:
+            return False
+
+    def at_goal_state(self):
+        '''checks if the current state is a goal state (red car is at the door)'''
+        # if red car points upward, return true if its in the second-topmost row
+        if self.directions[self.redCarIdx] == 1:
+            return (self.coords[self.redCarIdx][0] == 1)
+        
+        # else return true if its in the topmost row
+        return (self.coords[self.redCarIdx][0] == 0)
+        
+    def __repr__(self):
+        '''creates a neat string representation for the state'''
+        # store ' ' for an empty cell, otherwise a letter corresponding to the vehicle index (a, b, c...).
+        # the trunk coordinate letter will be capitalised, the others will be lowercase
+        grid = self.createGrid()
+
+        # make a neat string representation of this game state, along with border and door
         repr_string = ''
-        repr_string += '■' + '-'*self.door_col + ' ' + '-'*(self.m-self.door_col-1) + '■\n'
-        for row in repr_arr:
+        repr_string += '■' + '-'*self.doorCol + ' ' + '-'*(self.m-self.doorCol-1) + '■\n'
+        for row in grid:
             repr_string += '|' + ''.join(row) + '|\n'
         repr_string += '■' + '-'*self.m + '■'
         return repr_string
+    
+    def h1(self):
+        '''calculate heuristic 1: number of cells between red car and door'''
+        if self.directions[self.redCarIdx] == 1:
+            return (self.coords[self.redCarIdx][0] - 1)
+        else:
+            # else return true if its in the topmost row
+            return (self.coords[self.redCarIdx][0])
+
+    def h2(self):
+        '''calculate heuristic 2: number of cells between red car and door + number of those that are occupied'''
+        if self.directions[self.redCarIdx] == 1:
+            cost = (self.coords[self.redCarIdx][0] - 1)
+            
+        else:
+            cost = (self.coords[self.redCarIdx][0])
+        
+        return cost
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from game import CarSlidingGame
+from queue import PriorityQueue
 
-coordsA = [(0,3), (0,4), (1,4), (2,3),
-              (2,5), (3,0), (4,4), (4,5)]
-isTrucksA = [0, 0, 0, 1, 0, 0, 1, 0]
-directionsA = [3, 0, 0, 2, 2, 1, 2, 1]
-gameA = CarSlidingGame((6,6), 5, coordsA, isTrucksA, directionsA)
+coords = ((0,3), (0,4), (1,4), (2,3),
+              (2,5), (3,0), (4,4), (4,5))
+n = len(coords)
+isTrucks = [0, 0, 0, 1, 0, 0, 1, 0]
+directions = [3, 0, 0, 2, 2, 1, 2, 1]
+doorIdx = 5
+redcar_index = 3 #change?
+gameA = CarSlidingGame((6,6), doorIdx, coords, isTrucks, directions)
+heur_choice = 1
 print(gameA)
 
 # coordsB = [(0,0), (0,1), (0,3), (1,2),
@@ -12,12 +17,83 @@ print(gameA)
 #             (4,4), (5,3)]
 # isTrucksB = [1, 1, 1, 0, 1, 0, 0, 0, 0, 0]
 # directionsB = [3, 3, 0, 1, 2, 2, 0, 2, 1, 2]
-# gameB = CarSlidingGame((6,6), 4, coordsB, isTrucksB, directionsB)
+# doorIdxB = 4
+# gameB = CarSlidingGame((6,6), doorIdxB, coordsB, isTrucksB, directionsB)
 # print(gameB)
 
 # coordsC = [(0,2), (0,5), (1,4), (2,1),
 #               (2,3), (3,0), (3,4), (3,5), (4,3)]
 # isTrucksC = [1, 1, 0, 1, 0, 1, 1, 0, 1]
 # directionsC = [3, 2, 0, 3, 1, 3, 2, 1, 0]
-# gameC = CarSlidingGame((6,6), 5, coordsC, isTrucksC, directionsC)
+# doorIdxC = 5
+# gameC = CarSlidingGame((6,6), doorIdxC, coordsC, isTrucksC, directionsC)
 # print(gameC)
+
+path = []
+
+def find_path(state):
+    #path.append(state)
+    while state is not None:
+        path.append(state)
+        state = parentDict[state]
+
+    path.reverse()
+
+q = PriorityQueue()
+visited = set()
+parentDict = dict() # key: state (list of coords), value: cheapest parent state of the given state
+# queue element format: (expected f(n)=cost from start to goal through state n, state)
+q.put((0, coords))
+while q:
+    dist, coords = q.pop()
+
+    #check if visited:
+    if coords in visited:
+        continue
+
+    thisConfig = CarSlidingGame((6,6), doorIdx, coords, isTrucks, directions)
+
+    #check if goal state:
+    if thisConfig.at_goal_state():
+        find_path(coords)
+        break
+    
+    #marking current state as visited:
+    visited.add(coords)
+
+    for action_i in range(2*n):
+        vehicle_index = action_i//2
+        move_direction = 2*(action_i % 2)-1
+
+        #calculating current heuristics:
+        if heur_choice == 1:
+            parent_heur = thisConfig.h1()
+
+        else: 
+            parent_heur = thisConfig.h2()
+
+        thisConfig.takeAction(vehicle_index, move_direction)
+        
+        # check visited
+        if thisConfig.coords in visited:
+            #undo here
+            thisConfig.takeAction(vehicle_index, -move_direction)
+            continue
+
+        # check conflict
+        if thisConfig.hasConflict():
+            thisConfig.takeAction(vehicle_index, -move_direction)
+            continue
+
+        #calculating new heuristics:
+        if heur_choice == 1:
+            heur = thisConfig.h1()
+
+        else: 
+            heur = thisConfig.h2()
+
+        q.put((dist - parent_heur + 1 + heur, thisConfig.coords))
+        
+        # undo the action taken so we can re-use 'thisConfig' to find future state neighbours
+        thisConfig.takeAction(vehicle_index, -move_direction)
+
